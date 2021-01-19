8161e79baae5eb256617c316b668d17054b404b2
Gerard Wilkinson <gerard@ignite.systems>
2021-01-19 11:06:09 +0000

    Adding build step

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,20 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <lottie-animation data="@/assets/patch-loading.json" />
+    <lottie-animation data="@/assets/scratch-loading.json" />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import LottieAnimation from './components/LottieAnimation.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    LottieAnimation,
   }
 }
 </script>
---
+++ src/components/LottieAnimation.vue
@@ -0,0 +1,27 @@
+<template>
+  <div ref="loadingLottie"></div>
+</template>
+
+<script>
+import lottie from 'lottie-web';
+
+export default {
+  name: 'Loading',
+  props: {
+    data: Object,
+    lottie: Object,
+  },
+  mounted() {
+    this.lottie = lottie.loadAnimation({
+      container: this.$refs.loadingLottie,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: this.data,
+    });
+  },
+  destroy() {
+    this.lottie?.destroy();
+  }
+}
+</script>
